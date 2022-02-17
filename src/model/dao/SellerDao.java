c87bd2d7d1ee849236297b73ee1534f13d48439c
hbsmoura <humbertobsmoura@gmail.com>
2022-02-17 18:44:47 -0300

    Adding database access

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+	void insert(Seller sel);
+	void update(Seller sel);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+	List<Seller> findByDepartment(Department dep);
+}
